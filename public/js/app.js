587ba46e8558be14889311a11aec4ab911d51761
Jeremy Cerise <jcerise06@gmail.com>
2013-09-27 14:58:34 -0600

    Removed all traces of default Article content
    -MEAN ships with an example article model, removed all traces of it, as its not needed.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles',
+window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system',
   'mean.projects']);
 
 angular.module('mean.system', []);
-angular.module('mean.articles', []);
 angular.module('mean.projects', []);
